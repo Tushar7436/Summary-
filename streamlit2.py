c4de6d6c114bce7429079c6a19e6237f0138229a
Tushar Agarwal <128110806+Tushar7436@users.noreply.github.com>
2024-02-16 10:27:09 +0530

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -5,35 +5,31 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 import PyPDF2
-import io
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
 # Function to extract important sentences from a PDF based on keywords
-def extract_important_sentences_from_pdf(uploaded_file, keywords):
-    pdf_content = io.BytesIO(uploaded_file.read())
-    pdf_reader = PyPDF2.PdfFileReader(pdf_content)
+def extract_important_sentences_from_pdf(pdf_path, keywords):
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        important_sentences = []
 
-    important_sentences = []
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            sentences = sent_tokenize(page_text)
+            words = word_tokenize(page_text)
+            stop_words = set(stopwords.words('english'))
+            words = [word for word in words if word.lower() not in stop_words]
 
-    for page_number in range(pdf_reader.numPages):
-        page = pdf_reader.getPage(page_number)
-        page_text = page.extractText()
+            fdist = FreqDist(words)
 
-        sentences = sent_tokenize(page_text)
-        words = word_tokenize(page_text)
-        stop_words = set(stopwords.words('english'))
-        words = [word for word in words if word.lower() not in stop_words]
+            for sentence in sentences:
+                if any(keyword.lower() in sentence.lower() for keyword in keywords):
+                    important_sentences.append(sentence)
 
-        fdist = FreqDist(words)
-
-        for sentence in sentences:
-            if any(keyword.lower() in sentence.lower() for keyword in keywords):
-                important_sentences.append(sentence)
-
-    important_sentences_text = '\n'.join(important_sentences)
-    return important_sentences_text
+        important_sentences_text = '\n'.join(important_sentences)
+        return important_sentences_text
 
 # Streamlit app
 def main():
@@ -52,6 +48,12 @@ def main():
         st.subheader("Summarization of Document:")
         st.text(important_sentences)
 
-        # Display the first 500 characte
+        # Display the first 500 characters of important sentences
+        st.subheader("Important Sentences:")
+        st.text(important_sentences[:500])
+
+if __name__ == "__main__":
+    main()
+
 
 
